0cb97e5f1ad55f058f66cf57d087e314c5f6a67d
lrossetti <lrossetti7@gmail.com>
2021-11-13 10:35:55 -0500

    Cleaned Up Files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,22 @@
+import { makeStyles, Container } from '@material-ui/core';
 import React from 'react';
+import BuzzButton from '../components/buzz-button';
+import DecisionWheel from '../components/wheel';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+}));
 
 const Home = () => {
-  return <div>Home page</div>
+  const styles = useStyles();
+  return (
+    <Container className={styles.root}>
+      <DecisionWheel />
+      <BuzzButton />
+    </Container>
+  );
 }
+
 
 
 export default Home;
